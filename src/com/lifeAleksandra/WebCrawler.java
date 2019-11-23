634fc68fda0fcc1658d5f158c33271c3471053e4
panda020 <matuszek.aleksandra@gmail.com>
2019-11-23 11:22:47 +0100

    zmiany zmiany zmiany

--- src/com/lifeAleksandra/WebCrawler.java
+++ src/com/lifeAleksandra/WebCrawler.java
@@ -17,6 +17,7 @@ public class WebCrawler{
         Connection connect = Jsoup.connect("https://www.skapiec.pl/szukaj/w_calym_serwisie/"+product.name+"/price"); //pobranie zrodla strony
         Elements webSites; //strony 1.2...3
         //Elements oneLink; //brak opcji porownaj ceny
+        Elements comparePriceLink;
         do {
             Document document = connect.get();
             webSites = document.select("a.pager-btn.arrow.right"); //strong.price.gtm_sor_price //strony z wynikami wyszukiwania 1,2,3...
@@ -37,25 +38,25 @@ public class WebCrawler{
                 for (Element box1 : box) {
                     Float priceInBox = Float.parseFloat(box1.select("strong.price.gtm_sor_price").text().replace("od ", "").replace(" ", "").replace("zł", "").replace(",", "."));
                     if (priceInBox > product.min_price && priceInBox < product.max_price) {
-                        Elements comperePriceLink = box1.select("a.compare-link-1");//strona porownaj cene- kilka ofert na Skapiec.pl
-                        System.out.println(comperePriceLink.text());
-                        if(comperePriceLink.text().isEmpty()){
-                            comperePriceLink = box1.select("a.more-info");
-                            System.out.println("ygfu"+ comperePriceLink.text());
+                        comparePriceLink = box1.select("a.compare-link-1");//strona porownaj cene- kilka ofert na Skapiec.pl
+                        System.out.println(comparePriceLink.text());
+                        if(comparePriceLink.text().isEmpty()){
+                            comparePriceLink = box1.select("a.more-info");
+                            System.out.println("ygfu"+ comparePriceLink.text());
                         }
                         System.out.println(priceInBox);
-                        connect = Jsoup.connect("https://www.skapiec.pl" + comperePriceLink.attr("href"));
+                        System.out.println("https://www.skapiec.pl"+comparePriceLink.attr("href"));
+                        connect = Jsoup.connect("https://www.skapiec.pl" + comparePriceLink.attr("href"));
                         //System.out.println("https://www.skapiec.pl" + comperePriceLink1.attr("href"));
                         document = connect.get();
                         Elements square = document.select("a.offer-row-item.gtm_or_row");
                         for (Element square1 : square) {
                             Elements price = square1.select("span.price.gtm_or_price");
-                            if (!price.text().isEmpty()) {
+                            if (!price.text().isEmpty()) {  //dla stron bez porownaj ceny
                                 float floatPrice = Float.parseFloat(price.text().replace(",", ".").replace(" zł", "").replace(" ", ""));
-
                                 if (product.min_price < floatPrice && floatPrice < product.max_price) {
                                     Elements numberOfOpinions = square1.select("span.counter");
-                                    if (!numberOfOpinions.text().isEmpty()) {  //spr czy są brak opinii
+                                    if (!numberOfOpinions.text().isEmpty()) {  //spr czy są "brak opinii"
                                         if (Integer.parseInt(numberOfOpinions.text()) >= 50) { //warunek na ilosc opinii
                                             Elements reputation = square1.select("span.stars.green");  //.span.stars.green
                                             if (Float.parseFloat(reputation.attr("style").replace("width: ", "").replace("%", "")) >= product.reputation * 100 / 5) {//gwiazdki
@@ -92,7 +93,7 @@ public class WebCrawler{
 
 
     public static void main(String[] args) {
-        Product p = new Product("mysz logitech g502", 1, 250, 400, 4);
+        Product p = new Product("mysz logitech g502", 1, 280, 400, 4);
         WebCrawler w = new WebCrawler();
         try {
             w.Search(p);
