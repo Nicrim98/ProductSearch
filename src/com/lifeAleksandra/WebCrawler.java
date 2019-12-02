f8f764250754dcfd1256a7d8c93a9e0c842dcd0c
panda020 <matuszek.aleksandra@gmail.com>
2019-12-02 15:54:14 +0100

    watki w Crawlerze11

--- src/com/lifeAleksandra/WebCrawler.java
+++ src/com/lifeAleksandra/WebCrawler.java
@@ -49,91 +49,6 @@ public class WebCrawler {
                     break;
                 }
             }
-
-//                Float priceInBox = Float.parseFloat(box1.select("strong.price.gtm_sor_price").text().replace("od ", "").replace(" ", "").replace("zł", "").replace(",", "."));
-//                if (priceInBox >= product.min_price - 0.1 * product.min_price && priceInBox <= product.max_price) {
-//                    Elements comparePriceLink = box1.select("a.compare-link-1");//strona porownaj cene- kilka ofert na Skapiec.pl
-//                    if (comparePriceLink.text().isEmpty()) {
-//                        comparePriceLink = box1.select("a.more-info"); //dla stron bez porownaj ceny
-//                    }
-//                    connect = Jsoup.connect("https://www.skapiec.pl" + comparePriceLink.attr("href"));
-//                    document = connect.get();
-//                    Elements rectangle = document.select("a.offer-row-item.gtm_or_row");
-//                    for (Element square1 : rectangle) {
-//                        float theBestDeliveryPrice = 1000;
-//                        Elements price = square1.select("span.price.gtm_or_price");
-//                        String productName = square1.select("span.description.gtm_or_name").text();
-//                        if (!price.text().isEmpty()) {
-//                            float floatPrice = Float.parseFloat(price.text().replace(",", ".").replace(" zł", "").replace(" ", ""));
-//                            if (product.min_price <= floatPrice && floatPrice <= product.max_price) {
-//                                Elements numberOfOpinions = square1.select("span.counter");
-//                                if (!numberOfOpinions.text().isEmpty()) {  //spr czy są "brak opinii"
-//                                    if (Integer.parseInt(numberOfOpinions.text()) >= 50) { //warunek na ilosc opinii
-//                                        Elements reputation = square1.select("span.stars.green");  //.span.stars.green
-//                                        float foundReputation = Float.parseFloat(reputation.attr("style").replace("width: ", "").replace("%", ""));
-//                                        if (foundReputation >= product.reputation * 100 / 5) {//gwiazdki
-//                                            Elements url = square1.select("a.offer-row-item.gtm_or_row");
-//                                            String foundUrl = "https://www.skapiec.pl" + url.attr("href");
-//                                            //pobieranie kosztu dostawy dla każdego produktu "prostokąta na stronie skapiec -> porownaj ceny lub wiecej info jesli nie ma porownaj ceny"
-//                                            Elements delivery = square1.select("a.delivery-cost.link.gtm_oa_shipping");
-//                                            //darmowa dostawa
-//                                            if (delivery.text().isEmpty()) {
-//                                                theBestDeliveryPrice = 0;
-//                                                //System.out.println("Najtansza dostawa: " + theBestDeliveryPrice);
-//                                            } else {
-//                                                //koszt dostawy != darmowa dostawa
-//                                                String delivery1 = delivery.attr("href");
-//                                                connect = Jsoup.connect("https://www.skapiec.pl" + delivery1);
-//                                                document = connect.get();
-//                                                //sprawdzanie ceny dostawy w zakladkach Poczta, Kurier itd (bez odbior osobisty)
-//                                                String selector = "#product_content > ul > li:nth-child(0) > a";  //trzeba podmieniać (x) na 1,2,4,5 zeby przejsc po zakladkach
-//                                                for (int i = 1; i < 6; i++) {
-//                                                    int previousI = i - 1;
-//                                                    if (i == 3) {
-//                                                        i++;
-//                                                    }
-//                                                    selector = selector.replace(String.valueOf(previousI), String.valueOf(i));
-//                                                    Elements sub = document.select(selector);
-//                                                    connect = Jsoup.connect("https://www.skapiec.pl/delivery.php" + sub.attr("href"));
-//                                                    //System.out.println("https://www.skapiec.pl/delivery.php" + sub.attr("href"));
-//                                                    document = connect.get();
-//                                                    Elements deliveryPrice = document.select("#deliveryRulesets > tbody > tr.even > td:nth-child(2) > div:nth-child(1) > b");
-//                                                    Elements deliveryPrice1 = document.select("#deliveryRulesets > tbody > tr.odd > td:nth-child(2) > div > b");
-//                                                    for (Element deliveryPrice2 : deliveryPrice) {
-//                                                        String p = deliveryPrice2.text().replace(" zł", "");
-//                                                        float actualDeliveryPrice = Float.parseFloat(p);
-//                                                        if (theBestDeliveryPrice > actualDeliveryPrice) {
-//                                                            theBestDeliveryPrice = actualDeliveryPrice;
-//                                                        }
-//                                                    }
-//                                                    for (Element deliveryPrice2 : deliveryPrice1) {
-//                                                        String p = deliveryPrice2.text().replace(" zł", "");
-//                                                        float actualDeliveryPrice = Float.parseFloat(p);
-//                                                        if (theBestDeliveryPrice > actualDeliveryPrice) {
-//                                                            theBestDeliveryPrice = actualDeliveryPrice;
-//                                                        }
-//                                                    }
-//                                                }
-//                                                //System.out.println("Najlepsza dostawa: "+ theBestDeliveryPrice);
-//                                            }
-//                                            //jesli nie darmowa dostawa i brak info o cenie za dostawe
-//                                            if (theBestDeliveryPrice == 1000) {
-//                                                break;
-//                                            }
-//                                            //Tu bym zrobiła tylko dodanie produktu do arrayListy
-//
-//                                            listOfProducts.add(new FoundProduct(productName, floatPrice, theBestDeliveryPrice, foundReputation, foundUrl));
-//
-//                                        }
-//                                    }
-//                                }
-//                            }
-//                        }
-//                    }
-//                }
-//           }
-
-
             for (Element elem : webSites) {
                 connect = Jsoup.connect("https://www.skapiec.pl" + elem.attr("href"));
             }
@@ -249,5 +164,4 @@ public class WebCrawler {
             System.out.println((end.getTime()-start.getTime())/1000);
     }
     }
-    //jhegfiuhgrjto
 
