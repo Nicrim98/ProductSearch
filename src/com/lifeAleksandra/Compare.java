824ce32e2ba5c0f20f17de5356c94fa6b6765f36
Marcin <mduplagan@hotmail.com>
2019-12-02 16:28:33 +0100

    SET/COMPARE

--- src/com/lifeAleksandra/Compare.java
+++ src/com/lifeAleksandra/Compare.java
@@ -1,13 +1,68 @@
 package com.lifeAleksandra;
 
-public class Compare {
+import java.util.ArrayList;
 
-    public Compare() {
-    }
+public class Compare{
 
-    public FoundProduct[][] check(FoundProduct[][] tmp, int numberOfProducts) {
-        FoundProduct[][] final_p = tmp;         // FoundProduct[wariacja cenowa produktu][dany produkt]
+    protected int numberOfSets = 5;
+    protected int counter = 0;
 
-        return final_p;
+    public FoundProduct[][] check(FoundProduct[][] defaultSet, int numberOfProducts, ArrayList<Integer> shopIDs, float priceSet1, float priceSet2, float priceSet3) {
+
+        FoundProduct[][] beforeCheck = defaultSet;         // FoundProduct[wariacja cenowa produktu][dany produkt]
+
+        FoundProduct[][] tmp = null;
+
+        ArrayList<Integer> duplicateShopIDs = new ArrayList<Integer>();
+
+        for(int i = 0; i < shopIDs.size()-1 ; i++) {
+            for(int j = i+1; j < shopIDs.size(); j++){
+                if(shopIDs.get(i).equals(shopIDs.get(j)) && !duplicateShopIDs.contains(shopIDs.get(i))){
+                    duplicateShopIDs.add(shopIDs.get(i));   // lista sklepów, które mają różne produkty i są one w zestawieniach
+                }
+            }
+        }
+
+        // stworzenie tych zesatawień z tańszą dosatwą i porównanie z domyślnie najtańszymi zestawami XD
+
+        // zastanowić się nad braniem odpowiednich produktów z zestawień do porównań shopID
+        for(int IDs : duplicateShopIDs){    // dla każdego zduplikowanego shopid szukamy powiązanych produktówz danym jednym sklepie
+            float maxDeliveryPrice = 0;
+            float totalPricePromisingSet = 0;
+            boolean foundIDinproducts = false;
+
+            for(int i=0; i < numberOfProducts; i++) {
+                for (int j=0; j < numberOfSets; j++) {
+                    if(defaultSet[j][i] != null) {
+                        if (defaultSet[j][i].shopId == IDs) {
+                            if (defaultSet[j][i].foundDeliveryPrice > maxDeliveryPrice) {
+                                maxDeliveryPrice = defaultSet[j][i].foundDeliveryPrice;
+                                defaultSet[j][i].foundDeliveryPrice = 0;
+                                tmp[counter][i] = defaultSet[j][i];
+                                totalPricePromisingSet += tmp[counter][i].foundProductTotalPrice;
+                                foundIDinproducts = true;
+                                break;
+                            }
+                        }if( (j+1) == numberOfSets && foundIDinproducts == false){
+                            tmp[counter][i] = defaultSet[j][i];   // jeżeli nie ma danego produktu w powiązannych sklepach to bierzemy najtańszy dany produkt :P
+                            totalPricePromisingSet += tmp[counter][i].foundProductTotalPrice;
+                        }
+                        if ((i + 1) == numberOfSets && (j + 1) == numberOfProducts) { // znalezione wszystkie powiązane produkty z danym sklepem to zliczamy cenę za zestaw
+                            totalPricePromisingSet += maxDeliveryPrice;
+                            //Set s = new Set(tmp, totalPricePromisingSet);
+                        }
+                    }
+                }
+            }
+            counter += 1;
+        }
+
+        /*for(int i=0; i < counter; i++){
+            if (tmp[counter][i]){
+
+            }
+        }
+        */
+        return tmp;
     }
 }
--- src/com/lifeAleksandra/Set.java
+++ src/com/lifeAleksandra/Set.java
@@ -3,7 +3,7 @@ package com.lifeAleksandra;
 import java.io.IOException;
 import java.util.ArrayList;
 
-public class Set {
+public class Set implements Runnable{
     // klasa która będzie zbierać zestawienia produktów
     // sets[0][] / sets[1][] / set[2][] <- set0 najlepszy itd.
 
@@ -11,15 +11,30 @@ public class Set {
     // wyświetlania lepszego, bo nie będziemy brać wszystkich kolejnych produktów tylko jeden produkt będzie się różnił ceną
 
     // następnie w klasie Compare zamierzam patrzeć na id_sklepu i sprawdzać wtedy jeszcze tą dostawę w przypadku tego samego sklepu
-    protected final int numberOfSets = 3;
+    protected final int numberOfSets = 5;
+    protected float priceSet1 = 0;
+    protected float priceSet2 = 0;
+    protected float priceSet3 = 0;
+
+    protected ArrayList<Integer> shopIDs = new ArrayList<>();
+
+    public Set(){
+    }
+
+   /* public void run(WebCrawler web, Product p, FoundProduct[] options) {
+        try {
+            options = web.Search(p, 5);     // load best 3 options for the product[number of product]
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    };
+    */
 
     public FoundProduct[][] makeSets(Product[] p, int numberOfProducts){
 
         WebCrawler web = new WebCrawler();
         ArrayList<FoundProduct> options = new ArrayList<FoundProduct>(5);   // 5 najlepszych opcji dostajemy od webcrawlera
-        FoundProduct[][] sets = new FoundProduct[3][numberOfProducts];  // wybiermay 3 zestawy z wyszukiwanych produktów
-
-
+        FoundProduct[][] sets = new FoundProduct[5][numberOfProducts];  // wybiermay 3 zestawy z wyszukiwanych produktów
 
         // tworzenie zestawiń (domyślnie najlepszych bez patrzenia czy produkty z tego samego sklepu)
         for(int i=0; i < numberOfProducts; i++) {
@@ -29,12 +44,28 @@ public class Set {
             } catch (IOException e) {
                 e.printStackTrace();
             }
+            sets[numberOfSets - 5][i] = options.get(0);
+            shopIDs.add(options.get(0).shopId);
+            priceSet1 += options.get(0).getFoundProductTotalPrice();    // zliczanie ceny za zestaw, tylko dla trzech pierwszych, bo tylko 3 zestawienia końcowe
+            sets[numberOfSets - 4][i] = options.get(1);
+            shopIDs.add(options.get(1).shopId);
+            priceSet2 += options.get(1).getFoundProductTotalPrice();
+            sets[numberOfSets - 3][i] = options.get(2);     // Najlepsze zestawienie czytaj najtańsze (webcrawler zwraca posortowane od najtańszego)
+            shopIDs.add(options.get(2).shopId);
+            priceSet3 += options.get(2).getFoundProductTotalPrice();
+            sets[numberOfSets - 2][i] = options.get(3);     // 2 najlepsze zestawienie
+            shopIDs.add(options.get(3).shopId);
+            sets[numberOfSets - 1][i] = options.get(4);     // ostatnine zestawienie
+            shopIDs.add(options.get(4).shopId);
 
-            sets[numberOfSets-3][i] = options.get(0);     // Najlepsze zestawienie czytaj najtańsze (webcrawler zwraca posortowane od najtańszego)
-            sets[numberOfSets-2][i] = options.get(1);     // 2 najlepsze zestawienie
-            sets[numberOfSets-1][i] = options.get(2);     // ostatnine zestawienie
+            //Compare compare = new Compare();
+            //compare.check();
+
         }
-
         return sets;
+    }
+
+    @Override
+    public void run(){
     }
 }
