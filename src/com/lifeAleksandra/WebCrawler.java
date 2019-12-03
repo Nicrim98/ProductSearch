df31b6b15d686e82c6782157bbf38897a1a0084d
panda020 <matuszek.aleksandra@gmail.com>
2019-12-03 22:12:10 +0100

    efbvj

--- src/com/lifeAleksandra/WebCrawler.java
+++ src/com/lifeAleksandra/WebCrawler.java
@@ -12,6 +12,7 @@ import java.util.ArrayList;
 
 public class WebCrawler {
 
+
     protected ArrayList<FoundProduct> listOfProducts = new ArrayList<FoundProduct>();
     protected ArrayList<FoundProduct> finalAnswer = new ArrayList<FoundProduct>();
 
@@ -23,16 +24,15 @@ public class WebCrawler {
         return listOfProducts;
     }
 
+    //funkcja wyszukujaca produkt na stronie Skapiec
     public ArrayList<FoundProduct> Search(Product product, int time) throws IOException {
 
         Connection connect = Jsoup.connect("https://www.skapiec.pl/szukaj/w_calym_serwisie/" + product.getName()); //pobranie zrodla strony
-        Elements webSites; //strony 1.23..
-       // ArrayList<FoundProduct> listOfProducts = new ArrayList<FoundProduct>();
+        Elements webSites; //strony 1,2,3..
         Timestamp start = new Timestamp(System.currentTimeMillis());
         boolean out = false;
 
         do {
-
             Document document = connect.get();
             webSites = document.select("a.pager-btn.arrow.right"); //strong.price.gtm_sor_price //strony z wynikami wyszukiwania 1,2,3...
             Elements box = document.select("div.box-row.js");
@@ -53,9 +53,7 @@ public class WebCrawler {
             for(Thread t : threads){
                 t.run();
                 Timestamp end = new Timestamp(System.currentTimeMillis());
-                System.out.println((end.getTime()-start.getTime())/1000);
                 if((end.getTime()-start.getTime())/1000>=time){
-                    System.out.println("break!!!!!");
                     out = true;
                     break;
                 }
@@ -72,13 +70,11 @@ public class WebCrawler {
         } while (webSites.size() == 1);
 
         System.out.println("Ile produktow: "+listOfProducts.size());
-
-        // Dodac sortowanie
+        //Sortowanie po cenie+wysylka znalezionych produktow
         Sort sort = new Sort();
         sort.buble(listOfProducts);
-        //sort.mergeSort(listOfProducts);
-        //listOfProducts.sort(listOfProducts.get().getFoundProductPrice());
 
+        //wybranie 5 najlepszych ofert
         for(int i = 0; i<5;i++){
             finalAnswer.add(listOfProducts.get(i));
         }
@@ -88,9 +84,9 @@ public class WebCrawler {
 
     }
 
-
+    //funkcja przeszukujaca poszczegolne boxy z ogolnej strony Skapiec
     public void SearchBox(Element box, Product product) throws IOException {
-
+        //pobranie ceny boxa
         Float priceInBox = Float.parseFloat(box.select("strong.price.gtm_sor_price").text().replace("od ", "").replace(" ", "").replace("zł", "").replace(",", "."));
         if (priceInBox >= product.min_price - 0.1 * product.min_price && priceInBox <= product.max_price) {
             Elements comparePriceLink = box.select("a.compare-link-1");//strona porownaj cene- kilka ofert na Skapiec.pl
@@ -100,6 +96,7 @@ public class WebCrawler {
             Connection connect = Jsoup.connect("https://www.skapiec.pl" + comparePriceLink.attr("href"));
             Document document = connect.get();
             Elements rectangle = document.select("a.offer-row-item.gtm_or_row");
+            //pobieranie informacji ze strony szczegolowej - cena, ilosc opinii, ilosc gwiazdek, koszt dostawy,url produktu
             for (Element square1 : rectangle) {
                 float theBestDeliveryPrice = 1000;
                 Elements price = square1.select("span.price.gtm_or_price");
@@ -108,7 +105,7 @@ public class WebCrawler {
                     float floatPrice = Float.parseFloat(price.text().replace(",", ".").replace(" zł", "").replace(" ", ""));
                     if (product.min_price <= floatPrice && floatPrice <= product.max_price) {
                         Elements numberOfOpinions = square1.select("span.counter");
-                        if (!numberOfOpinions.text().isEmpty()) {  //spr czy są "brak opinii"
+                        if (!numberOfOpinions.text().isEmpty()) {  //spr czy jest "brak opinii"
                             if (Integer.parseInt(numberOfOpinions.text()) >= 50) { //warunek na ilosc opinii
                                 Elements reputation = square1.select("span.stars.green");  //.span.stars.green
                                 float foundReputation = Float.parseFloat(reputation.attr("style").replace("width: ", "").replace("%", ""));
@@ -161,8 +158,6 @@ public class WebCrawler {
                                     if (theBestDeliveryPrice == 1000) {
                                         break;
                                     }
-                                    //Tu bym zrobiła tylko dodanie produktu do arrayListy
-
                                     listOfProducts.add(new FoundProduct(productName, floatPrice, theBestDeliveryPrice, foundReputation, foundUrl));
 
                                 }
